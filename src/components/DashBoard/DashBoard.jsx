02b71004d896a1fbdc47dfeaffefb6c237a131c8
Neeraj Patel <neeraj9838spsd@gmail.com>
2024-11-12 15:53:31 +0530

    first commit

---
+++ src/components/DashBoard/DashBoard.jsx
@@ -0,0 +1,217 @@
+import { React } from "react";
+import { useSelector } from "react-redux";
+import { AiOutlinePlus } from "react-icons/ai";
+
+import { MdCancel } from "react-icons/md";
+
+import { DiCodeigniter } from "react-icons/di";
+import {
+  BsFillCheckCircleFill,
+} from "react-icons/bs";
+import "./DashBoard.css";
+import Card from "../Card/Card";
+const DashBoard = () => {
+  const isStatus = localStorage.getItem("group") === "status";
+  const isPriority = localStorage.getItem("group") === "priority";
+  console.log("stat", isStatus, "prio", isPriority);
+  const { selectedData, user } = useSelector(
+    (state) => state.SelectDataReducer
+  );
+  console.log("rere", user);
+  return (
+    selectedData && (
+      <div
+        className="dashContainer"
+        style={{ display: "flex", justifyContent: "space-around" }}
+      >
+        {selectedData.map((element, index) => {
+          const cardWidthPercentage = 18.7;
+          return (
+            <div
+              key={index}
+              className="dashCardContainer"
+              style={{ width: `${cardWidthPercentage}%` }}
+            >
+              <div className="dashCardHeading flex-sb">
+                <div className="leftView">
+                  {user ? (
+                    <div
+                      className="imageContainer relative"
+                      style={{
+
+                        width: "10px",
+                        height: "15px",
+                        display: "inline-block",
+                      }}
+                    >
+                    </div>
+                  ) : isStatus ? (
+                    <div
+                      className="cardTitle"
+                      style={{
+                        width: "15px",
+                        height: "15px",
+                        display: "inline-block",
+                        fontWeight: 200,
+                      }}
+                    >
+                      {element[index].title === "Backlog" ? (
+                        <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M7 13C10.3137 13 13 10.3137 13 7C13 3.68629 10.3137 1 7 1C3.68629 1 1 3.68629 1 7C1 10.3137 3.68629 13 7 13Z" stroke="#95999F" stroke-width="2" stroke-dasharray="1.4 1.74"/>
+                        </svg>
+                      )
+                        : element[index].title === "Todo" ? (
+                          <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M7 13C10.3137 13 13 10.3137 13 7C13 3.68629 10.3137 1 7 1C3.68629 1 1 3.68629 1 7C1 10.3137 3.68629 13 7 13Z" stroke="#B8B8B8" stroke-width="2"/>
+                          </svg>
+
+                        ) : element[index].title === "In progress" ? (
+                          <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M7 13C10.3137 13 13 10.3137 13 7C13 3.68629 10.3137 1 7 1C3.68629 1 1 3.68629 1 7C1 10.3137 3.68629 13 7 13Z" fill="white" stroke="#F2BE00" stroke-width="2"/>
+                          <path d="M9 7C9 5.89543 8.10457 5 7 5C5.89543 5 5 5.89543 5 7C5 8.10457 5.89543 9 7 9C8.10457 9 9 8.10457 9 7Z" stroke="#F2BE00" stroke-width="4"/>
+                          </svg>
+
+                        ) : element[index].title === "Done" ? (
+                          <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M7 13C10.3137 13 13 10.3137 13 7C13 3.68629 10.3137 1 7 1C3.68629 1 1 3.68629 1 7C1 10.3137 3.68629 13 7 13Z" fill="#5E6AD2" stroke="#5E6AD2" stroke-width="2"/>
+                          <path d="M10 7C10 5.34315 8.65685 4 7 4C5.34315 4 4 5.34315 4 7C4 8.65685 5.34315 10 7 10C8.65685 10 10 8.65685 10 7Z" stroke="#5E6AD2" stroke-width="6" stroke-dasharray="18.85 100"/>
+                          <path d="M10.951 4.24896C11.283 4.58091 11.283 5.11909 10.951 5.45104L5.95104 10.451C5.61909 10.783 5.0809 10.783 4.74896 10.451L2.74896 8.45104C2.41701 8.11909 2.41701 7.5809 2.74896 7.24896C3.0809 6.91701 3.61909 6.91701 3.95104 7.24896L5.35 8.64792L9.74896 4.24896C10.0809 3.91701 10.6191 3.91701 10.951 4.24896Z" fill="#FBFBFB"/>
+                          </svg>
+
+                        ) : (
+                          <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M7 13C10.3137 13 13 10.3137 13 7C13 3.68629 10.3137 1 7 1C3.68629 1 1 3.68629 1 7C1 10.3137 3.68629 13 7 13Z" fill="#96A3B4" stroke="#96A3B4" stroke-width="2"/>
+                          <path d="M10 7C10 5.34315 8.65685 4 7 4C5.34315 4 4 5.34315 4 7C4 8.65685 5.34315 10 7 10C8.65685 10 10 8.65685 10 7Z" stroke="#96A3B4" stroke-width="6" stroke-dasharray="18.85 100"/>
+                          <path d="M6.99682 7.68089L4.81753 9.86018C4.72008 9.95763 4.60934 10.0041 4.48532 9.99971C4.36129 9.99528 4.25055 9.94434 4.15311 9.84689C4.05566 9.74945 4.00693 9.6365 4.00693 9.50804C4.00693 9.37959 4.05566 9.26663 4.15311 9.16919L6.31911 7.00318L4.13982 4.82388C4.04237 4.72643 3.99586 4.61348 4.00029 4.48503C4.00472 4.35657 4.05566 4.24362 4.15311 4.14617C4.25055 4.04872 4.36351 4 4.49196 4C4.62041 4 4.73337 4.04872 4.83081 4.14617L6.99682 6.32547L9.17612 4.14617C9.27357 4.04872 9.38652 4 9.51497 4C9.64343 4 9.75638 4.04872 9.85383 4.14617C9.95128 4.24362 10 4.35657 10 4.48503C10 4.61348 9.95128 4.72643 9.85383 4.82388L7.67453 7.00318L9.85383 9.18247C9.95128 9.27992 10 9.39066 10 9.51468C10 9.63871 9.95128 9.74945 9.85383 9.84689C9.75638 9.94434 9.64343 9.99307 9.51497 9.99307C9.38652 9.99307 9.27357 9.94434 9.17612 9.84689L6.99682 7.68089Z" fill="#E8EAED"/>
+                          </svg>
+
+                        )}
+                    </div>
+                  ) : isPriority ? (
+                    <div
+                      className="tags color-grey"
+                      style={{
+                        width: "35px",
+                        height: "30px",
+                        display: "inline-block",
+                      }}
+                    >
+                      {element[index].title === "Low" ||
+                        element[index].title === "Medium" ||
+                        element[index].title === "High" ? (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+
+                          width="24"
+                          height="24"
+                          fill="currentColor"
+                          className="bi bi-signal"
+                          viewBox="0 0 16 16"
+                        >
+                          <rect x="1" y="10" width="3" height="2" />
+                          <rect
+                            x="5"
+                            y="7"
+                            width="3"
+                            height="5"
+                            opacity={
+                              element[index].title === "Medium" ||
+                                element[index].title === "High"
+                                ? 1
+                                : 0.25
+                            }
+                          />
+                          <rect
+                            x="9"
+                            y="4"
+                            width="3"
+                            height="8"
+                            opacity={element[index].title === "High" ? 1 : 0.25}
+                          />
+                        </svg>
+                      ) : element[index].title === "Urgent" ? (
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M3 1C1.91067 1 1 1.91067 1 3V13C1 14.0893 1.91067 15 3 15H13C14.0893 15 15 14.0893 15 13V3C15 1.91067 14.0893 1 13 1H3ZM7 4H9L8.75391 8.99836H7.25L7 4ZM9 11C9 11.5523 8.55228 12 8 12C7.44772 12 7 11.5523 7 11C7 10.4477 7.44772 10 8 10C8.55228 10 9 10.4477 9 11Z" fill="#FB773F"/>
+                        </svg>
+
+                      ) : (
+                        <p></p>
+                      )}
+                    </div>
+                  ) : (
+                    <DiCodeigniter />
+                  )}{" "}
+                  <span>
+                    {element[index]?.title} {element[index].value?.length}
+                  </span>
+                </div>
+                <div className="rightView">
+                  <AiOutlinePlus />{" "}
+                  <span style={{ letterSpacing: "2px" }}>...</span>
+                </div>
+              </div>
+              <div className="dashList flex-gap-10">
+                {element[index]?.value?.map((element, ind) => {
+                  return (
+                    <Card
+                      id={element.id}
+                      title={element.title}
+                      tag={element.tag}
+                      status={element.status}
+                      priority={element.priority}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+        {isStatus && (
+          <>
+            <div className="dashCardHeading flex-sb">
+              <div className="leftView" style={{ fontSize: "15px", marginRight: "90px", wordSpacing: "4px" }}>
+                <div
+                  className="cardTitle"
+                  style={{
+                    width: "13px",
+                    height: "13px",
+                    display: "inline-block",
+                    fontWeight: 200,
+                  }}
+                >
+                  <BsFillCheckCircleFill style={{ color: "blue" }} />
+                </div>{" "}
+                <span style={{ fontSize: "13px", fontWeight: "lighter" }}>Done</span> <span style={{ fontSize: "13px", color: "#8F9997" }}>0</span>
+              </div>
+              <div className="rightView">
+                <AiOutlinePlus />{" "}
+                <span style={{ letterSpacing: "2px" }}>...</span>
+              </div>
+            </div>
+            <div className="dashCardHeading flex-sb">
+              <div className="leftView" style={{ fontSize: "15px", marginRight: "60px", wordSpacing: "4px" }}>
+                <div
+                  className="cardTitle"
+                  style={{
+                    width: "9px",
+                    height: "9px",
+                    display: "inline-block",
+                    fontWeight: 200,
+                  }}
+                >
+                  <MdCancel style={{ color: "grey" }} />
+                </div>{" "}
+                <span style={{ fontSize: "13px", fontWeight: "lighter" }}>Canceled</span> <span style={{ fontSize: "13px", color: "#8F9997" }}>0</span>
+              </div>
+              <div className="rightView">
+                <AiOutlinePlus />{" "}
+                <span style={{ letterSpacing: "2px" }}>...</span>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    )
+  );
+};
+export default DashBoard;
